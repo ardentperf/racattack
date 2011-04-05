038e2d817f149a654d162acb079e881863513589
Jeremy Schneider <jeremy.schneider@ardentperf.com>
2011-04-05 16:53:11 -0500

    sources for custom DVD contents

---
+++ makeDVD/root/fix_cssd/fix_cssd.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+while true
+do
+  for CHECK in [o]cssd [c]ssdmonitor [c]ssdagent; do
+    PROC=$(ps ax|grep $CHECK)
+    if [[ -n "$PROC" ]]
+    then
+      if [[ -n "$(echo $PROC|awk '{print$3}'|grep L)" ]]
+      then
+        OPID=$(echo $PROC|awk '{print$1}')
+        gdb -p $OPID <<"        EOF"
+          call munlockall()
+          quit
+        EOF
+      fi
+    fi
+  done
+  sleep 60
+done
+
