90312ea41589c34730b58e158134cb4118a847a1
Jeremy Schneider <jeremy.schneider@ardentperf.com>
2011-09-20 16:28:59 -0500

    Add support for prompting of jumpstart variables

--- setupenv.bat
+++ setupenv.bat
@@ -18,6 +18,8 @@ REM along with RAC-ATTACK. If not, see <http://www.gnu.org/licenses/>.
 REM
 REM
 
+set PROMPTENV=YES
+
 set SOURCEDRIVE=D:
 
 set DESTDRIVE=C:
@@ -29,6 +31,54 @@ set DESTDIRSHARED=RAC11g-shared
 set MYUSER=%USERNAME%
 set MYPASS=%YOUR_WINDOWS_PASSWORD%
 
-set VMRUNBIN="C:\program files\vmware\vmware server\vmrun"
-set LZOPBIN=d:\lzop
-set MD5BIN=d:\md5sum
+set VMWAREDIR="C:\Program Files\VMware\VMware Server"
+
+
+REM ===============================================================
+REM Begin prompting section
+
+if /i %PROMPTENV% neq YES goto endprompt
+
+echo s/DEFAULT-SOURCEDRIVE/%SOURCEDRIVE%/          >%TEMP%\UserIn.sed
+echo s/DEFAULT-DESTDRIVESHARED/%DESTDRIVESHARED%/ >>%TEMP%\UserIn.sed
+echo s/DEFAULT-DESTDRIVE/%DESTDRIVE%/             >>%TEMP%\UserIn.sed
+echo s/DEFAULT-VMWAREDIR/%VMWAREDIR%/             >>%TEMP%\UserIn.sed
+echo s/DEFAULT-MYUSER/%MYUSER%/                   >>%TEMP%\UserIn.sed
+echo s/DEFAULT-MYPASS/%MYPASS%/                   >>%TEMP%\UserIn.sed
+
+sed "s/\\/\\\\/g" %TEMP%\UserIn.sed >%TEMP%\UserIn2.sed
+sed -f %TEMP%\UserIn2.sed input.hta >%TEMP%\UserIn.hta
+start /w %TEMP%\UserIn.hta
+call %TEMP%\UserIn.bat
+
+del %TEMP%\UserIn.sed
+del %TEMP%\UserIn2.sed
+del %TEMP%\UserIn.hta
+del %TEMP%\UserIn.bat
+
+@echo off
+echo. 
+echo SOURCEDRIVE=%SOURCEDRIVE%
+echo DESTDRIVE=%DESTDRIVE%
+echo DESTDRIVESHARED=%DESTDRIVESHARED%
+echo VMWAREDIR=%VMWAREDIR%
+echo MYUSER=%MYUSER%
+echo MYPASS=%MYPASS%
+echo. 
+@echo on
+
+:endprompt
+
+
+REM ===============================================================
+REM Vars that are dependant on other vars
+
+set VMRUNBIN="%VMWAREDIR:"=%\vmrun"
+set LZOPBIN=%SOURCEDRIVE%\lzop.exe
+set MD5BIN=%SOURCEDRIVE%\md5sum
+
+if not exist %LZOPBIN% (
+  echo ERROR: bad source drive!
+  pause
+  exit 1
+)
