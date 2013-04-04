667204fce00ab94160524943f5571382fc9bb564
Jeremy Schneider <jeremy.schneider@ardentperf.com>
2013-04-04 16:09:47 -0500

    fixed bug where all.py patch woudn't apply

--- makebook/setup.sh
+++ makebook/setup.sh
@@ -3,7 +3,7 @@
 sudo apt-get -y update
 sudo apt-get -y install vnc4server twm xterm synaptic chromium-browser
 sudo apt-get -y install subversion ghc libghc6-regex-compat-dev libghc6-http-dev python3.2 texlive-latex-extra texlive-fonts-extra librsvg2-bin imagemagick texlive-games texlive-fonts-recommended texlive-lang-cyrillic texlive-lang-greek arabtex texlive-science texlive-humanities python3.2-tk texlive-full libghc6-missingh-dev ttf-freefont cm-super libghc6-split-dev cabal-install libghc6-hxt-dev latex-cjk-all cm-super-minimal ttf-unifont libghc6-utility-ht-dev ttf-wqy-zenhei python-fontforge ttf-mph-2b-damase
-sudo apt-get -y install poppler-utils python3-pyqt4 p7zip-full pdftk texmaker
+sudo apt-get -y install poppler-utils python3-pyqt4 p7zip-full dos2unix pdftk texmaker
 
 cabal update
 cd
@@ -26,6 +26,7 @@ wget http://upload.wikimedia.org/wikipedia/commons/5/52/Racattack-book-title.png
 
 cd ../../
 wget https://raw.github.com/ardentperf/racattack/master/makebook/wb2pdf-racattack.diff
+dos2unix src/all.py
 patch -b -p0 -i wb2pdf-racattack.diff
 
 cd ../..
